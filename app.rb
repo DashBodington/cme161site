37a1261c2aa9882b36dde0d813e4db6c356f8c55
Dave Deriso <deriso@gmail.com>
2015-01-04 23:50:09 -0800

    added server

---
+++ app.rb
@@ -0,0 +1,23 @@
+# app.rb
+require 'sinatra'
+
+get '/' do
+  "Hello World!"
+end
+
+# CRUD routes
+post '/' do
+#  .. create something ..
+end
+
+get '/' do
+#  .. read something ..
+end
+
+put '/' do
+#  .. update something ..
+end
+
+delete '/' do
+#  .. destroy something ..
+end
