e35bef1a9c63674a4022ced135ad697d085ae2a1
Dave Deriso <deriso@gmail.com>
2016-05-08 23:14:15 -0700

    simpler server

--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -1,34 +1,21 @@
 import os, copy
-debug = False
-
-from flask import Flask, jsonify, request, send_from_directory, url_for, redirect, make_response
-from flask.ext.cors import CORS
+from flask import Flask, jsonify, request, send_from_directory, make_response
 app = Flask(__name__, static_url_path='')
-CORS(app)
 
 # get root
 @app.route("/")
 def index():
     return app.make_response(open('app/index.html').read())
 
-# send assets
+# send assets (ex. assets/js/random_triangle_meshes/random_triangle_meshes.js)
+# blocks other requests, so your directories won't get listed (ex. assets/js will return "not found")
 @app.route('/assets/<path:path>')
-# @nocache
 def send_assets(path):
     return send_from_directory('app/assets/', path)
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5050))
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port, debug=False)
 
-
-
-# prevent changes from being ignored due to caching, enable this and @nocache if you need to
-# from flask.ext import assets
-# from functools import update_wrapper
-# def nocache(f):
-# 	def new_func(*args, **kwargs):
-# 		resp = make_response(f(*args, **kwargs))
-# 		resp.cache_control.no_cache = True
-# 		return resp
-# 	return update_wrapper(new_func, f)
+# set debug=True if you want to have auto-reload on changes
+# this is great for developing
