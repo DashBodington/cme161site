60a4f52d93b85772418ed7d5c2a83cae432e1b58
DashBodington <dashbodington@gmail.com>
2016-06-02 15:47:53 -0700

    endpoints

--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -29,3 +29,55 @@ if __name__ == "__main__":
 
 # set debug=True if you want to have auto-reload on changes
 # this is great for developing
+
+import json, collections
+@app.route('/trellis', methods=['GET'])
+def get_trellis():
+	# This method should return the entire data
+	# Replace the following line with your own code
+	with open('app/assets/data/trellis.json') as data_file:
+		return json.dumps(json.load(data_file))
+
+@app.route('/trellis/limit/<int:n_entries>', methods=['GET'])
+def get_trellis_limit(n_entries):
+	# This method should return only the first 'n_entries' entries
+	# Replace the following line with your own code
+	with open('app/assets/data/trellis.json') as data_file:
+		return json.dumps(json.load(data_file)[:n_entries])
+
+def make_data_graph(data_list_in):
+	# This method should convert the raw trellis data into the graph format
+	# Replace the following line with your own code
+	idx = 0
+	names = collections.OrderedDict()
+	for e in data_list_in:
+		to = e['to'][:7] # truncate
+		fr = e['from'][:7]
+		if to not in names:
+			names[to] = idx
+			idx += 1
+		if fr not in names:
+			names[fr] = idx
+			idx += 1
+	edges = [{
+				"source": names[e['to'][:7]], 
+				"target": names[e['from'][:7]], 
+				"value": e['n'], 
+				"tags":  [d['tag'] for d in e['data']] 
+			} for e in data_list_in
+		]
+	nodes = [{"name":n} for n in names.keys()]
+	return { "nodes": nodes, "edges": edges }
+
+@app.route('/graph', methods=['GET'])
+def get_graph():
+	# This method should return the entire converted graph data
+	# Replace the following line with your own code
+	with open('app/assets/data/trellis.json') as data_file:
+		return json.dumps(make_data_graph(json.load(data_file)))
+
+@app.route('/graph/limit/<int:n_entries>', methods=['GET'])
+def get_graph_limit(n_entries):
+	# This method should convert and return only the first 'n_entries' entries
+	# Replace the following line with your own code
+	return json.dumps(make_data_graph(json.load(data_file)[:n_entries]))
